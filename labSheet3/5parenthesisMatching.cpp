98b0d5513160b29d62f9f501b26dfd9a7f1934a7
Chirath <chirath.02@gmail.com>
2016-01-24 21:31:54 +0530

    labsheet 3 5th que done

--- labSheet3/5parenthesisMatching.cpp
+++ labSheet3/5parenthesisMatching.cpp
@@ -31,6 +31,10 @@ public:
         return 0;
     }
     
+    char getTop() {
+        return arr[top];
+    }
+    
     int topIs() {
         return top;
     }
@@ -51,17 +55,29 @@ void printISEmpty(bool b) {
         cout << "Stack is not Empty" << endl;
 }
 
+bool isPair(char a, char b) {
+    if(b=='(' && a == ')')
+        return true;
+    if(b=='{' && a == '}')
+        return true;
+    if(b=='[' && a == ']')
+        return true;
+    return false;
+}
+
 bool checkBalanced(char exp[]) {
-    n = strlen(exp);
+    int n = strlen(exp);
+    char temp;
     MyStack S;
     for(int i=0; i < n;++i) {
-        if(exp[i]=='('||'['||'{') 
+        if(exp[i]=='('||exp[i]=='['||exp[i]=='{') 
             S.push(exp[i]);
         else 
-            if(exp[i]==')'||'}'||']')
-                char temp = S.pop()
-                if(S.topIs() == -1 || !pair(exp[i], temp)
+            if(exp[i]==')'||exp[i]=='}'||exp[i]==']')
+                if(S.topIs() == -1 || !isPair(exp[i], S.getTop()))
                     return false;
+                else 
+                    S.pop();
     }
     return S.topIs()==-1?true:false;
 }
@@ -71,7 +87,7 @@ int main() {
     char arr[100], length;
     cout << "Enter expression" << endl;
     cin >> arr;
-    if(checkBalanced())
+    if(checkBalanced(arr))
         cout << "Expersion is balanced" << endl;
     else
         cout << "Expression is not balanced" << endl;
